fc78c1d3486514c646989ed60382d2117f835d33
shaunam1 <S00273772@atu.ie>
2025-12-11 11:47:27 +0000

    moved some internal script files to external script files

---
+++ public/forgotPasswordScript.js
@@ -0,0 +1,51 @@
+//New Password
+const newPasswordMessageDiv = document.getElementById('newPasswordMessage');
+const submitNew = document.getElementById('submitNewPasswordBtn');
+submitNew.addEventListener('click', newPasswordEmailValid);
+        
+
+        function newPasswordEmailValid(){
+            newPasswordMessageDiv.innerHTML = "";
+            let isValid = false;
+            if (email.value.includes("@")){
+                isValid = true;
+                
+                comparePasswords();
+            }
+            else{
+                isValid = false;
+            }
+        }
+
+        function comparePasswords(){
+            event.preventDefault();
+        
+            if (passwordID.value == reenterPasswordID.value){
+                newPasswordMessageDiv.innerHTML = "";
+                const successMessage = document.createElement('p');
+                successMessage.innerHTML = "Password reset";
+                newPasswordMessageDiv.appendChild(successMessage);
+                setNewPasswordJSON();
+            }
+            else{
+                newPasswordMessageDiv.innerHTML = "";
+                const failMessage = document.createElement('p');
+                failMessage.innerHTML = "Passwords do not match. Try again";
+                newPasswordMessageDiv.appendChild(failMessage);
+            }
+        }
+
+        function setNewPasswordJSON(){
+            const email = document.getElementById('email');
+            const emailValue = email.value;
+            const password = document.getElementById('passwordID');
+            const passwordDetails = password.value;
+            const userLoginDetails = 
+            {
+                "email":emailValue,
+                "password":passwordDetails 
+            }
+
+            localStorage.setItem("loginDetails", JSON.stringify(userLoginDetails));
+
+        }
---
+++ public/loginScript.js
@@ -0,0 +1,117 @@
+//Login or Logout
+let loginOrLogout = document.getElementById('log');
+if (sessionStorage.getItem('loginState')){
+    loginOrLogout.innerHTML = sessionStorage.getItem('loginState');
+    
+}
+else{
+    loginOrLogout.innerHTML = "Login";
+}
+const json = localStorage.getItem('loginDetails');
+const obj = JSON.parse(json); 
+const loginButton = document.getElementById('loginBtn');
+loginButton.addEventListener('click', emailValid);
+const emailAddress = document.getElementById('loginEmail');
+const messageDiv = document.getElementById('loginMessage');
+const loginPassword = document.getElementById('loginPasswordID');
+const disappear = document.getElementById('disappear');
+const disappear2 = document.getElementById('disappear2');
+let isLoggedIn = false;
+let state = sessionStorage.getItem('loginState');
+const logout = document.getElementById('logoutBtn');
+logout.addEventListener('click', logUserOut);
+
+(function newFunction(){
+    if(state){
+        if(state === "Login"){
+    //if logged in
+    loginButton.style.display = "block";
+    logout.style.display = "none";
+    disappear.style.display = "block";
+    disappear2.style.display = "block";
+
+}
+else{
+    loginButton.style.display = "none";
+    logout.style.display = "block";
+    disappear.style.display = "none";
+    disappear2.style.display = "none";
+    
+
+}
+
+
+    }
+    else{
+    loginButton.style.display = "block";
+    logout.style.display = "none";
+    disappear.style.display = "block";
+    disappear2.style.display = "block";
+}
+    
+})();
+
+        
+function emailValid(event){
+    messageDiv.innerHTML = "";
+    let isValid = false;
+    if (emailAddress.value.includes("@")){
+        isValid = true;
+        compareToRegisteredDetails(event);
+    }
+    else{
+        isValid = false;
+    }
+}
+
+function compareToRegisteredDetails(event){
+    event.preventDefault();
+    messageDiv.innerText = "";
+    if((emailAddress.value == obj.email) && (loginPassword.value == obj.password)){
+        const successMessage = document.createElement('p');
+        successMessage.innerHTML = "You are now logged in."
+        messageDiv.appendChild(successMessage);
+        isLoggedIn = true;
+        
+        if(loginButton.style.display === "none" && logoutBtn.style.display === "display"){
+        loginButton.style.display = "block";
+        logout.style.display = "none";
+        disappear.style.display = "block";
+        disappear2.style.display = "block";
+    }
+    else{
+         loginButton.style.display = "none";
+         logout.style.display = "block";
+         disappear.style.display = "none";
+         disappear2.style.display = "none";
+       
+    }
+    changeLoginState();
+    }
+    else{
+        messageDiv.innerText = "";
+        const failMessage = document.createElement('p');
+        failMessage.innerHTML = "Email or password is incorrect."
+        messageDiv.appendChild(failMessage);
+    }
+}
+
+function logUserOut(){
+    loginButton.style.display = "block";
+        logout.style.display = "none";
+        disappear.style.display = "block";
+        disappear2.style.display = "block";
+        isLoggedIn = false;
+         changeLoginState();
+}
+
+function changeLoginState(){
+    if(isLoggedIn === true){
+        loginOrLogout.innerHTML = "Logout";
+        sessionStorage.setItem('loginState', 'Logout');
+    }
+    else{
+         loginOrLogout.innerHTML = "Login";
+         sessionStorage.setItem('loginState', 'Login');
+    }  
+}
